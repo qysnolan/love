f58aff0598341eb0b990b35ae88f81531590dcf4
qysnolan <qysnolan@gmail.com>
2015-08-16 21:20:35 -0500

    add

--- love/urls.py
+++ love/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'love.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', RedirectView.as_view(pattern_name='home')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^home/', 'love.views.home', name='home'),
+    url(r'^menu/', 'menu.views.home', name='menu'),
 ]
